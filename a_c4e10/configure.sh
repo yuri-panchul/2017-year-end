5cf525e40c8476b6cd6414451a9d34706685179c
yuri.panchul <yuri@panchul.com>
2018-03-02 23:20:43 -0800

    Added checking for two cables

--- a_c4e10/configure.sh
+++ a_c4e10/configure.sh
@@ -2,18 +2,28 @@
 
 . ./setup.sh
 
-CABLE_NAME=`quartus_pgm -l | grep "1) " | sed -e 's/1) //'`
-
 if [ ! -d "$SYN_DIR" ]
 then
     echo "Synthesis directory ${SYN_DIR} does not exist. Run synthesis first."
     exit 1
 fi
 
-if [ "$CABLE_NAME" ]
+cd $SYN_DIR
+
+quartus_pgm -l > cable_list
+
+CABLE_NAME_1=`grep "1) " cable_list | sed -e 's/1) //'`
+CABLE_NAME_2=`grep "2) " cable_list | sed -e 's/2) //'`
+
+if [ "$CABLE_NAME_1" ]
 then
-    cd $SYN_DIR
-    quartus_pgm --no_banner -c "$CABLE_NAME" --mode=jtag -o "P;top.sof"
+    if [ "$CABLE_NAME_2" ]
+    then
+        echo "Warning: more than one cable is connected: ${CABLE_NAME_1} and ${CABLE_NAME_2}"
+    fi
+
+    echo "Using cable ${CABLE_NAME_1}"
+    quartus_pgm --no_banner -c "$CABLE_NAME_1" --mode=jtag -o "P;top.sof"
 else
     echo "Cannot detect a USB-Blaster cable connected"
     exit 1
