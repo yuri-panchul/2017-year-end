af22cd5b7294b48c65b699fb06bbd4110b768043
yuri.panchul <yuri@panchul.com>
2018-03-02 23:07:56 -0800

    Bug in setup

--- a_c4e10/setup.sh
+++ a_c4e10/setup.sh
@@ -5,6 +5,6 @@ set -e
 #export QUARTUS_ROOTDIR=${HOME}/altera/13.0sp1/quartus
 export QUARTUS_ROOTDIR=${HOME}/intelFPGA_lite/17.1/quartus
 
-export PATH=${PATH}:${QUARTUS_ROOTDIR}
+export PATH=${PATH}:${QUARTUS_ROOTDIR}/bin
 
 SYN_DIR=${PWD}/syn
