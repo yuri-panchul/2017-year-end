cabc5b90f6a7cc8310a7ac035aee1fed6ddf40c3
yuri.panchul <yuri@panchul.com>
2018-03-03 21:56:13 -0800

    Added FSMs

--- a_c4e10/top.v
+++ a_c4e10/top.v
@@ -11,6 +11,8 @@ module top
 
     wire rst_n = key [3];
 
+    //------------------------------------------------------------------------
+
     wire [3:0] key_db;
     wire [7:0] sw_db;
 
@@ -20,6 +22,79 @@ module top
     sync_and_debounce # (.w (8)) i_sync_and_debounce_sw
         (clk, ~ sw, sw_db);
 
+    //------------------------------------------------------------------------
+
+    wire shift_register_strobe;
+
+    strobe_gen # (.w (23)) i_shift_strobe
+        (clk, rst_n, shift_strobe);
+
+    wire [11:0] out_reg;
+
+    shift_register # (.w (12)) i_shift_reg
+    (
+        .clk     ( clk          ),
+        .rst_n   ( rst_n        ),
+        .en      ( shift_strobe ),
+        .in      ( key_db [1]   ),
+        .out_reg ( out_reg      )
+    );
+
+    assign led = ~ out_reg;
+
+    //------------------------------------------------------------------------
+
+    moore_fsm i_moore_fsm
+    (
+        .clk   ( clk           ),
+        .rst_n ( rst_n         ),
+        .en    ( shift_strobe  ),
+        .a     ( out_reg       ),
+        .y     ( out_moore_fsm )
+    );
+    
+    counter # (8) i_moore_fsm_out_counter
+    (
+        .clk   ( clk                 ),
+        .rst_n ( rst_n               ),
+        .en    ( out_moore_fsm       ),
+        .count ( moore_fsm_out_count )
+    );
+
+    //------------------------------------------------------------------------
+
+    mealy_fsm i_mealy_fsm
+    (
+        .clk   ( clk           ),
+        .rst_n ( rst_n         ),
+        .en    ( shift_strobe  ),
+        .a     ( out_reg       ),
+        .y     ( out_mealy_fsm )
+    );
+    
+    counter # (8) i_mealy_fsm_out_counter
+    (
+        .clk   ( clk                 ),
+        .rst_n ( rst_n               ),
+        .en    ( out_mealy_fsm       ),
+        .count ( mealy_fsm_out_count )
+    );
+
+    //------------------------------------------------------------------------
+
+    wire [15:0] shift_strobe_counter;
+    wire [ 7:0] moore_fsm_out_count;
+    wire [ 7:0] mealy_fsm_out_count;
+    
+    wire [31:0] number_to_display =
+    {
+        shift_strobe_counter,
+        moore_fsm_out_count,
+        mealy_fsm_out_count
+    };
+
+    //------------------------------------------------------------------------
+
     wire seven_segment_strobe;
 
     strobe_gen # (.w (10)) i_seven_segment_strobe
@@ -27,33 +102,18 @@ module top
 
     seven_segment #(.w (32)) i_seven_segment
     (
-        .clk     ( clk                            ),
-        .rst_n   ( rst_n                          ),
-        .en      ( seven_segment_strobe           ),
-        .num     ( { sw_db, sw_db, sw_db, sw_db } ),
-        .dots    ( sw_db                          ),
-        .abcdefg ( abcdefgh [7:1]                 ),
-        .dot     ( abcdefgh [0]                   ),
-        .anodes  ( digit                          )
+        .clk     ( clk                  ),
+        .rst_n   ( rst_n                ),
+        .en      ( seven_segment_strobe ),
+        .num     ( number_to_display    ),
+        .dots    ( sw_db                ),
+        .abcdefg ( abcdefgh [7:1]       ),
+        .dot     ( abcdefgh [0]         ),
+        .anodes  ( digit                )
     );
 
-    wire shift_register_strobe;
+    //------------------------------------------------------------------------
 
-    strobe_gen # (.w (23)) i_shift_register_strobe
-        (clk, rst_n, shift_register_strobe);
-
-    wire [11:0] out_reg;
-
-    shift_register # (.w (12)) i_shift_reg
-    (
-        .clk     ( clk                   ),
-        .rst_n   ( rst_n                 ),
-        .en      ( shift_register_strobe ),
-        .in      ( key_db [1]            ),
-        .out_reg ( out_reg               )
-    );
-
-    assign led    = ~ out_reg;
     assign buzzer = ~ key_db [0];
 
 endmodule
