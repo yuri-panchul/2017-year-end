f467d68d858cf48e702d56ef34d4ff9eba150965
Yuri Panchul <yuri@panchul.com>
2019-04-09 21:23:18 -0700

    Updated signals in Icarus script

--- a_c4e6e10/xx_gtkwave.tcl
+++ a_c4e6e10/xx_gtkwave.tcl
@@ -1,12 +1,15 @@
 # gtkwave::loadFile "dump.vcd"
 
 set all_signals [list]
-lappend all_signals tb.CLK
-lappend all_signals tb.D
-lappend all_signals tb.Q
-lappend all_signals tb.Q1
-lappend all_signals tb.Q2
-lappend all_signals tb.Q3
+
+lappend all_signals testbench.clk
+lappend all_signals testbench.key
+lappend all_signals testbench.sw
+lappend all_signals testbench.led
+lappend all_signals testbench.abcdefgh
+lappend all_signals testbench.digit
+lappend all_signals testbench.buzzer
+
 set num_added [ gtkwave::addSignalsFromList $all_signals ]
 
 gtkwave::/Time/Zoom/Zoom_Full
